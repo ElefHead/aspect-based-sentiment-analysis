a6f46dfc0f71242b9c2ccaa075833d42aa4c327d
Ganesh Jagadeesan <ganesh.jcs@gmail.com>
2018-04-08 03:50:07 -0500

    Fixed svm, kinda surprised by the accuracy achieved. Seems pretty fishy

--- svm_model.py
+++ svm_model.py
@@ -1,13 +1,13 @@
 from sklearn.svm import SVC
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, TfidfTransformer
-from sklearn.model_selection import GridSearchCV, train_test_split, StratifiedKFold, cross_val_score
-from sklearn.metrics import make_scorer, accuracy_score, f1_score
-from sklearn.metrics import confusion_matrix, recall_score, precision_score
-from sklearn.pipeline import make_pipeline, Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.model_selection import GridSearchCV, train_test_split, StratifiedKFold
 from nltk import word_tokenize
 from spacy import load
 import numpy as np
 import pandas as pd
+from utils import accuracy, precision, recall, f1
+import _pickle as cPickle
+from os import listdir, path
 
 np.random.seed(1)
 
@@ -20,34 +20,89 @@ def tokenize(text):
     return word_tokenize(text)
 
 
-def tfidfSVM(data_train, y_train):
-    vectorizer = CountVectorizer(ngram_range=(1,1),
-                                 analyzer='word',
-                                 tokenizer=tokenize)
+def train(x_train, y_train, check_trained=True, ngram_range=(1,1), vector_type="count"):
+    fname = 'svm_'+vector_type+"_vectorized_model_"+"_".join([str(i) for i in ngram_range])+".pkl"
+    if check_trained :
+        for file in listdir('./ml_models/'):
+            if file == fname:
+                print("Model present, returning trained model")
+                with open('./ml_models/'+fname,'rb') as fid:
+                    return cPickle.load(fid)
 
-    pipeline_svm = make_pipeline(vectorizer,
-                                 SVC(probability=True, kernel="linear", class_weight="balanced"))
-
-    grid_svm = GridSearchCV(pipeline_svm,
-                            param_grid={'svc__C': [0.01, 0.1, 1],
-                                        'svc__kernel': ['linear', 'rbf'],
-                                        },
+    cls = SVC(class_weight="balanced", random_state=0)
+    grid_cls = GridSearchCV(cls,
+                            param_grid={
+                                'C': [0.01, 0.1, 1],
+                                'kernel': ['linear', 'rbf']
+                            },
                             cv=kfolds,
-                            verbose=1,
                             n_jobs=-1
                             )
-    grid_svm.fit(data_train, y_train)
-    print(grid_svm.score(data_train, y_train))
+    grid_cls.fit(x_train, y_train)
+
+    with open(path.join('./ml_models/',fname), 'wb') as fid:
+        cPickle.dump(grid_cls.best_estimator_, fid)
+
+    return grid_cls.best_estimator_
+
+
+def predict(model, x_train):
+    return model.predict(x_train)
+
+
+def vectorized_svm(x_joint, y_joint, x_elec, y_elec, x_food, y_food, checktrain=True, ngram_range=(1, 1), vector_type="count"):
+    vectorizer = CountVectorizer(tokenizer=tokenize, ngram_range=ngram_range) if vector_type=="count" \
+        else TfidfVectorizer(tokenizer=tokenize, ngram_range=ngram_range)
+    vectorized_x_joint = vectorizer.fit_transform(x_joint)
+
+    x_train, x_test, y_train, y_test = train_test_split(vectorized_x_joint, y_joint, test_size=0.2, random_state=0,
+                                                        shuffle=True)
+
+    model = train(x_train, y_train, checktrain, ngram_range, vector_type=vector_type)
+    pred_x_train = predict(model, x_train)
+    pred_x_test = predict(model, x_test)
+    pred_elec = predict(model, vectorizer.transform(x_elec))
+    pred_food = predict(model, vectorizer.transform(x_food))
+
+    print("Accuracy training accuracy ("+vector_type, " vectorized joint data) =", accuracy(y_train, pred_x_train))
+    print("Accuracy testing accuracy ("+vector_type, "vectorized joint data) =", accuracy(y_test, pred_x_test),"\n")
+    print("Accuracy electronics data ("+vector_type, "vectorized) =", accuracy(y_elec, pred_elec))
+    print("Accuracy food data (" + vector_type, "vectorized) =", accuracy(y_food, pred_food),"\n")
+
+    # Set confusion matrix for elec data and then compute precision, recall and f1_score
+    precision_elec = precision(y_elec, pred_elec)
+    recall_elec = recall(y_elec, pred_elec)
+    f1_elec = f1(y_elec, pred_elec)
+
+    # Set confusion matrix for food data and then compute precision, recall and f1_score
+    precision_food = precision(y_food, pred_food)
+    recall_food = recall(y_food, pred_food)
+    f1_food = f1(y_food, pred_food)
+
+    print("Precision ("+vector_type+" vectorized electricity data) =", precision_elec)
+    print("Recall ("+vector_type+" electricity data) =", recall_elec)
+    print("F1 ("+vector_type+" electricity data) =", f1_elec, "\n")
+
+    print("Precision ("+vector_type+" food data) =", precision_food)
+    print("Recall ("+vector_type+" food data) =", recall_food)
+    print("F1 ("+vector_type+" food data) =", f1_food,"\n\n")
 
 
 def main():
-    data = pd.read_csv("./data/joint_data.csv")
-    X = data['filtered_text'].values
-    Y = data['class']
-    print(X[:5])
-    data_train, y_train, data_test, y_test = train_test_split(X, Y, test_size=0.1, random_state=1)
-    print(data_train[:5])
-    # tfidfSVM(data_train, y_train)
+    joint_data = pd.read_csv('./data/joint_data.csv')
+    elec_data = pd.read_csv('./data/elec_data_clean.csv')
+    food_data = pd.read_csv('./data/food_data_clean.csv')
+
+    x_joint = joint_data['filtered_text'].values.astype('U')
+    y_joint = joint_data['class'].values.astype(np.float32)
+    x_elec = elec_data['filtered_text'].values.astype('U')
+    y_elec = elec_data['class'].values.astype(np.float32)
+    x_food = food_data['filtered_text'].values.astype('U')
+    y_food = food_data['class'].values.astype(np.float32)
+
+    vectorized_svm(x_joint, y_joint, x_elec, y_elec, x_food, y_food, ngram_range=(1,1), vector_type="count")
+    vectorized_svm(x_joint, y_joint, x_elec, y_elec, x_food, y_food, ngram_range=(1,1), vector_type="tfidf")
+
 
 
 if __name__ == '__main__':
